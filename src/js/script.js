6942cefb901bcfc848f901745424c6869e98b858
Serhii <serg.shumskiy@mail.ru>
2020-07-14 13:25:14 +0300

    js

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/* 
+let number = 5;  */
+/* const leftBorderWidth = 1; */
+
+/* number = 10;
+console.log(number);
+
+const persone =`Alex`;
+console.log(persone); */
+
+/* const obj = {
+    name: "Alex",
+    age: 25,
+    isMarried: false
+};
+console.log(obj); */
+
+/* alert (`Hello`); */
+/* const result = confirm("Are you here?");
+console.log(result); */
+/* const answer = prompt("Are you here?", "");
+console.log(answer); */
+/* const answers = [];
+answers[0] = prompt("Как ваше имя ", "");
+answers[1] = prompt("Как ваше фамилия", "");
+answers[2] = prompt("Как ваше дела", "");
+
+document.write(answers); */
+
+
